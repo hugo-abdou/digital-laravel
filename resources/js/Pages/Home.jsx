e5e72c320a087c7f73f06df409cc3d325751e016
hugo-abdou <moikinge3@gmail.com>
2022-01-08 14:47:57 +0100

    .

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "../components/Card";
 import InfluenceManagement from "../components/InfluenceManagement";
 import OveralProgress from "../components/OveralProgress";
 import PersonaDetails from "../components/PersonaDetails";
@@ -9,6 +10,30 @@ const Home = function () {
             <PersonaDetails className="row-span-2 min-h-[200px]" />
             <InfluenceManagement className="min-h-[200px]" />
             <OveralProgress className="min-h-[200px]" />
+
+            <Card
+                title="Persona Statement - Background"
+                className="col-span-2 min-h-[200px]"
+            >
+                <div className="text-sm text-dark-gray space-y-3">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nunc, et sagittis, sit viverra elementum. Cursus gravida
+                        id gravida in risus quis. Sem et risus sed magnis
+                        nec.Nunc, et sagittis, sit viverra elementum. Cursus
+                        gravida id gravida in risus quis. Sem et risus sed
+                        magnis nec.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nunc, et sagittis, sit viverra elementum. Cursus gravida
+                        id gravida in risus quis. Sem et risus sed magnis
+                        nec.Nunc, et sagittis, sit viverra elementum. Cursus
+                        gravida id gravida in risus quis. Sem et risus sed
+                        magnis nec.
+                    </p>
+                </div>
+            </Card>
         </div>
     );
 };
